bc5b057ae37f9085b1290bc170f648753ab7a655
Jake <jkspivells42015@gmail.com>
2026-07-16 09:50:10 -0400

    Updated readme to reflect changes

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,21 +1,11 @@
-import { auth0 } from "@/lib/auth0";
-import LoginButton from "@/components/LoginButton";
-import LogoutButton from "@/components/LogoutButton";
-import Profile from "@/components/Profile";
-import Image from "next/image";
-
-
-const LoggedOutHeader:string = "Welcome to next-app"
-const LoggedOutMessage: string = "Get started by logging in"
+import { LoginCardWithoutAuth0, LoginCardWithAuth0 } from "@/components/LoginCard";
 
 export default async function Home() {
-  const session = await auth0.getSession();
-  const user = session?.user;
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-
+          {"Pick one: <LoginCardWithoutAuth0 /> or <LoginCardWithAuth0 />"}
       </div>
       </main>
     </div>
